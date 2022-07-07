0943f41a92c353dc37afbe49e208c1a572dada9f
abdelrahman <abdelrhman.ahmed.nassar@gmail.com>
2022-07-07 21:41:42 +0200

    add files

---
+++ 02-Udemy/-04-React/-05-Rendering-lists-&-conditional-content/expenses-app-project/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
+
+const DUMMY_EXPENSES = [
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 400,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Book",
+    amount: 294.67,
+    date: new Date(2020, 2, 28),
+  },
+  {
+    id: "e6",
+    title: "New Mentor",
+    amount: 350,
+    date: new Date(2019, 6, 12),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+};
+
+export default App;
---
+++ 02-Udemy/-04-React/-05-Rendering-lists-&-conditional-content/expenses-app-project/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import "./ExpenseForm.css";
+
+const ExpenseForm = (props) => {
+  // states
+  const [enteredDate, setEnteredDate] = useState("");
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+
+  // setting states
+  const titleChangeHandler = (e) => {
+    setEnteredTitle(e.target.value);
+  };
+
+  const amountChangeHandler = (e) => {
+    setEnteredAmount(e.target.value);
+  };
+
+  const dateChangeHandler = (e) => {
+    setEnteredDate(e.target.value);
+  };
+
+  // listen submit
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: +enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    // passing data to parent el
+    props.onSaveExpenseData(expenseData);
+    // clear the inputs
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Price</label>
+          <input
+            type="number"
+            min="0.1"
+            step="0.1"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2030-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button type="submit">Add expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
